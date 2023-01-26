842f8d1279bc725fa95df87b6a6e3ec793fef6e0
Cyross Makoto <takuan45189@gmail.com>
2023-01-26 18:58:11 +0900

    first commit

---
+++ VegasScriptEditEventTime/EntryPoint.cs
@@ -0,0 +1,47 @@
+﻿using ScriptPortal.Vegas;
+using System;
+using System.Windows.Forms;
+using VegasScriptHelper;
+
+namespace VegasScriptEditEventTime
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            VegasScriptSettings.Load();
+            VegasHelper helper = VegasHelper.Instance(vegas);
+
+            try
+            {
+                TrackEvent trackEvent = helper.GetSelectedEvent();
+                VegasDuration duration = helper.GetEventTime(trackEvent);
+
+                EventTimeSetting dialog = new EventTimeSetting()
+                {
+                    StartTime = duration.StartTime.Nanos,
+                    TimeLength = duration.Length.Nanos
+                };
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    duration.StartTime.Nanos = dialog.StartTime;
+                    duration.Length.Nanos = dialog.TimeLength;
+                    helper.SetEventTime(trackEvent, duration);
+                }
+            }
+            catch (VegasHelperTrackUnselectedException)
+            {
+                MessageBox.Show("トラックが選択されていません。");
+            }
+            catch (VegasHelperNoneEventsException)
+            {
+                MessageBox.Show("選択したトラック中にイベントが存在していません。");
+            }
+            catch(VegasHelperNoneSelectedEventException)
+            {
+                MessageBox.Show("イベントを選択していません。");
+            }
+
+        }
+    }
+}
---
+++ VegasScriptEditEventTime/EventTimeSetting.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace VegasScriptEditEventTime
+{
+    public partial class EventTimeSetting : Form
+    {
+        public EventTimeSetting()
+        {
+            InitializeComponent();
+        }
+
+        public long StartTime
+        {
+            get { return GetStartTimeNanos(); }
+            set { SetStartTimeNanos(value); }
+        }
+
+        public long TimeLength
+        {
+            get { return GetTimeLenthNanos(); }
+            set { SetTimeLengthNanos(value); }
+        }
+
+        private long RoundNanos(long nanos)
+        {
+            return (nanos + 5000) / 10000;
+        }
+
+        private void SetStartTimeNanos(long nanos)
+        {
+            SetNanos(nanos, startTimeHour, startTimeMinute, startTimeSecond, startTimeMilliSecond);
+        }
+
+        private void SetTimeLengthNanos(long nanos)
+        {
+            SetNanos(nanos, timeLengthHour, timeLengthMinute, timeLengthSecond, timeLengthMilliSecond);
+        }
+
+        private void SetNanos(long nanos, TextBox hBox, TextBox mBox, TextBox sBox, TextBox msBox)
+        {
+            long roundNanos = RoundNanos(nanos);
+            msBox.Text = (roundNanos % 1000).ToString();
+            roundNanos = roundNanos / 1000;
+            sBox.Text = (roundNanos % 60).ToString();
+            roundNanos = roundNanos / 60;
+            mBox.Text = (roundNanos % 60).ToString();
+            roundNanos = roundNanos / 60;
+            hBox.Text = roundNanos.ToString();
+        }
+
+        private long GetStartTimeNanos()
+        {
+            return GetNanos(startTimeHour, startTimeMinute, startTimeSecond, startTimeMilliSecond);
+        }
+
+        private long GetTimeLenthNanos()
+        {
+            return GetNanos(timeLengthHour, timeLengthMinute, timeLengthSecond, timeLengthMilliSecond);
+        }
+
+        private long GetNanos(TextBox hBox, TextBox mBox, TextBox sBox, TextBox msBox)
+        {
+            long nanos = long.Parse(hBox.Text) * 60;
+            nanos = (nanos + long.Parse(mBox.Text)) * 60;
+            nanos = (nanos + long.Parse(sBox.Text)) * 1000;
+            return (nanos + long.Parse(msBox.Text)) * 10000;
+        }
+
+
+        private void ValidateNumberOnly(object sender, CancelEventArgs e)
+        {
+            errorProvider1.Clear();
+            OKButton.Enabled = true;
+            ValidateNumBox(startTimeHour, 24);
+            ValidateNumBox(startTimeMinute, 60);
+            ValidateNumBox(startTimeSecond, 60);
+            ValidateNumBox(startTimeMilliSecond, 1000);
+            ValidateNumBox(timeLengthHour, 24);
+            ValidateNumBox(timeLengthMinute, 60);
+            ValidateNumBox(timeLengthSecond, 60);
+            ValidateNumBox(timeLengthMilliSecond, 1000);
+        }
+        
+        private void ValidateNumBox(TextBox box, long max)
+        {
+            int tmp = 0;
+            if (string.IsNullOrEmpty(box.Text))
+            {
+                box.Text = "0";
+            }
+            else if (!int.TryParse(box.Text, out tmp))
+            {
+                errorProvider1.SetError(box, "数値ではありません");
+                OKButton.Enabled = false;
+            }
+            else if (tmp < 0 || tmp >= max)
+            {
+                errorProvider1.SetError(box, "範囲外の数値です");
+                OKButton.Enabled = false;
+            }
+
+        }
+    }
+}
